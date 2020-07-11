4f253c666090a477d91a44a54c3db6a0a82fb88a
Benqqa <53792741+Benqqa@users.noreply.github.com>
2020-07-12 01:46:37 +0300

    Update move_card.js

--- salanport/scripts/move_card.js
+++ salanport/scripts/move_card.js
@@ -1,13 +1,11 @@
-var mouseX,mouseY;
 var mouse_x = mouse_y = 0;
-
 var flag;
 function init() {
     if (document.layers) document.captureEvents(Event.MOUSEMOVE);
     document.onmousemove = mousemove;
 }
+
 function mousemove(event) {
-    
     if (document.attachEvent != null) {
         mouse_x = window.event.clientX;
         mouse_y = window.event.clientY;
@@ -20,35 +18,23 @@ function mousemove(event) {
     my=mouse_y/window.innerHeight*100+'%';
     event.target.style.backgroundPosition =  mx+' '+my;
   }
-
 }
-
 init();
-
-
-
+card__content.onmousedown = function(e) {
+  
+}
 card__content.onmouseover = card__content.onmouseout = handler;
-
 function handler(event) {
 
   function str(el) {
     if (!el) return "null"
     return el.className || el.tagName;
   }
-
- /*if (event.type == 'mouseover') {
-    event.target.style.background = 'pink'
-  }
-  if (event.type == 'mouseout') {
-    event.target.style.background = ''
-  }*/
-
   if (event.type == 'mouseover') {
   flag=true;    
   }
-
   if (event.type == 'mouseout') {
     flag=false;
-    event.target.style.background = 'url("img/salamport.webp") no-repeat 50% 50%';
+    event.target.style.background = 'url("img/salamport.webp") no-repeat center';
   }
 }
